5c768e99803ee6ed661de56b30c1584e8c51c844
Gabor Vecsei <vecseigabor.x@gmail.com>
2017-01-29 15:01:14 +0100

    renames

---
+++ data/README.md
@@ -0,0 +1,3 @@
+# Data Folder
+
+Here I store the .csv file what I use for preparing the data and training
--- trainEmotionRecognizer.py
+++ trainEmotionRecognizer.py
@@ -1,5 +1,5 @@
-import time
 import os
+import time
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -43,7 +43,7 @@ train_image_shape = (1,) + image_shape
 print("Image shape: {0}".format(image_shape))
 print("Train image shape: {0}".format(train_image_shape))
 
-print ("Output layer dim: {0}".format(y_train.shape[1]))
+print("Output layer dim: {0}".format(y_train.shape[1]))
 
 batch_size = 256
 nb_epoch = 10
@@ -69,20 +69,22 @@ model.add(Dense(len(y_train.shape[1]), activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-checkPoint = ModelCheckpoint(os.path.join(CHECKPOINT_FOLDER_PATH, "weights-{epoch:02d}-{loss:.2f}-{acc:.2f}.hdf5"), monitor="loss",
+checkPoint = ModelCheckpoint(os.path.join(CHECKPOINT_FOLDER_PATH, "weights-{epoch:02d}-{loss:.2f}-{acc:.2f}.hdf5"),
+                             monitor="loss",
                              save_best_only=True,
                              save_weights_only=True)
-logTraining = LogTraining(os.path.join(VISUALIZATION_FOLDER_PATH, "training_log.txt"))
+logTraining = LogTraining(os.path.join(VISUALIZATION_FOLDER_PATH, "training_log_{0}_epochs.txt".format(nb_epoch)))
 callbacks = [checkPoint, logTraining]
 
 startTime = time.clock()
-hist = model.fit_generator(dataGenerator(batch_size, X_train, y_train), samples_per_epoch=20480, nb_epoch=nb_epoch, verbose=1,
+hist = model.fit_generator(dataGenerator(batch_size, X_train, y_train), samples_per_epoch=20480, nb_epoch=nb_epoch,
+                           verbose=1,
                            callbacks=callbacks)
 endTime = time.clock()
 
 print("Model is trained in {0} seconds!".format(endTime - startTime))
 
-print ("Evaluating the model...")
+print("Evaluating the model...")
 X_test = np.load(TEST_X_PATH).astype(np.float16)
 y_test = to_categorical(np.load(TEST_Y_PATH).astype(np.uint8))
 metrics = model.evaluate(X_test, y_test, batch_size=batch_size, verbose=1)
@@ -95,7 +97,7 @@ modelJson = model.to_json()
 with open(os.path.join(SAVE_MODEL_FOLDER_PATH, "model_structure.json", "w")) as json_file:
     json_file.write(modelJson)
 model.save_weights(os.path.join(SAVE_MODEL_FOLDER_PATH, "model_weights_{0}_epochs.h5".format(nb_epoch)))
-model.save(os.path.join(SAVE_MODEL_FOLDER_PATH, "trained_model.h5"))
+model.save(os.path.join(SAVE_MODEL_FOLDER_PATH, "trained_model_{0}_epochs.h5".format(nb_epoch)))
 print("Model is saved!")
 
 plt.figure(figsize=(20, 10))
@@ -103,11 +105,11 @@ plt.plot(hist.history['loss'])
 plt.title("Model loss")
 plt.xlabel("epoch")
 plt.legend(['loss'], loc='upper left')
-plt.savefig(os.path.join(VISUALIZATION_FOLDER_PATH, "train_loss_visualization.png"))
+plt.savefig(os.path.join(VISUALIZATION_FOLDER_PATH, "train_loss_visualization_{0}_epochs.png".format(nb_epoch)))
 
 plt.figure(figsize=(20, 10))
 plt.plot(hist.history['acc'])
 plt.title("Model acc")
 plt.xlabel("epoch")
 plt.legend(['acc'], loc='upper left')
-plt.savefig(os.path.join(VISUALIZATION_FOLDER_PATH, "train_acc_visualization.png"))
+plt.savefig(os.path.join(VISUALIZATION_FOLDER_PATH, "train_acc_visualization_{0}_epochs.png".format(nb_epoch)))
